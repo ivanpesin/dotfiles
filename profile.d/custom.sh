6ca3d6d3a56f38d7eb1db36331322e0a30db38ae
Ivan Pesin <ivan.pesin@gmail.com>
2017-10-22 21:24:36 -0400

    Update custom.sh

--- profile.d/custom.sh
+++ profile.d/custom.sh
@@ -3,7 +3,7 @@ if [ "$PS1" ]; then
   [ -f "/etc/systag.name" ]  && TAG="[$(cat /etc/systag.name)]"
   [ -f "/etc/systag.color" ] && TAG_COLOR="$(cat /etc/systag.color)"
 
-  PS1="$TAG_COLOR$TAG^[[0m[\u@\h \W]\\$ "
+  PS1="\[$TAG_COLOR\]$TAG\[\e[0m\][\u@\h \W]\\$ "
 
   HISTFILESIZE=1000000
   HISTSIZE=100000
